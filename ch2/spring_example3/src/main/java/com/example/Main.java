b87aeb91a74dcea285ada048de48ad9283438ed9
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-04 10:56:55 +0200

    example 3

---
+++ ch2/spring_example3/src/main/java/com/example/Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        // context.refresh();
+
+        Parrot p = context.getBean(Parrot.class);
+
+        System.out.println(p);
+        System.out.println(p.getName());
+    }
+}
