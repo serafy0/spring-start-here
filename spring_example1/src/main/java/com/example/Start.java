9fb9fdfbd187cc266c74e7def53d2ac4f8535ef5
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-04 08:07:44 +0200

    first commit

---
+++ spring_example1/src/main/java/com/example/Start.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Start {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot p = context.getBean(Parrot.class);
+
+        System.out.println(p.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
+
+    }
+}
