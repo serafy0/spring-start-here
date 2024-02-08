3c647686f218c6d91342b8628f5c3fa822a031b1
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-08 08:28:28 +0200

    3.2.2
    Using @Autowired to inject the values through the constructor

--- ch3/example1/src/main/java/com/example/Person.java
+++ ch3/example1/src/main/java/com/example/Person.java
@@ -8,8 +8,12 @@ public class Person {
 
     private String name = "Ella";
 
+    private final Parrot parrot;
+
     @Autowired
-    private Parrot parrot;
+    public Person(Parrot parrot) {
+        this.parrot = parrot;
+    }
 
     public String getName() {
         return name;
@@ -23,7 +27,4 @@ public class Person {
         return parrot;
     }
 
-    public void setParrot(Parrot parrot) {
-        this.parrot = parrot;
-    }
 }
