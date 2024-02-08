040c426b2f9c20469415eb7b8bd7a59c91994eb5
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-08 10:38:51 +0200

    3.4
    Using the Qualifier annotation

--- ch3/example1/src/main/java/com/example/ProjectConfig.java
+++ ch3/example1/src/main/java/com/example/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,10 +22,11 @@ public class ProjectConfig {
     }
 
     @Bean
-    public Person person(Parrot parrot2) {
+    public Person person(@Qualifier("parrot2") Parrot parrot) {
+
         Person p = new Person();
         p.setName("Ella");
-        p.setParrot(parrot2);
+        p.setParrot(parrot);
         return p;
     }
 
