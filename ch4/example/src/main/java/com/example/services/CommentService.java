56d033e4299ece0287fb38fb15e3a044d81de0fd
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-12 15:53:58 +0200

    4.2.2
    Choosing what to auto-wire from multiple implementations of an abstraction

--- ch4/example/src/main/java/com/example/services/CommentService.java
+++ ch4/example/src/main/java/com/example/services/CommentService.java
@@ -1,6 +1,7 @@
 package com.example.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.example.models.Comment;
@@ -10,13 +11,13 @@ import com.example.repositories.CommentRepo;
 @Component
 public class CommentService {
 
-    @Autowired
     private CommentRepo commentRepo;
-    @Autowired
     private CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepo commentRepo2, CommentNotificationProxy commentNotificationProxy2) {
-        // TODO Auto-generated constructor stub
+    public CommentService(CommentRepo commentRepo,
+            @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
+        this.commentNotificationProxy = commentNotificationProxy;
+        this.commentRepo = commentRepo;
     }
 
     public void publishComment(Comment comment) {
