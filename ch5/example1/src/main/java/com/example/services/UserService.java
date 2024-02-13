94f5b0eb7a7854582fba89a3199c86ac8383008e
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-13 12:32:47 +0200

    5.1.1

--- ch5/example1/src/main/java/com/example/services/UserService.java
+++ ch5/example1/src/main/java/com/example/services/UserService.java
@@ -1,9 +1,11 @@
 package com.example.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.repos.CommentRepository;
 
+@Service
 public class UserService {
 
     @Autowired
