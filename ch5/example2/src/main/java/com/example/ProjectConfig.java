7737bc018a2019d945e31274d9374e00640aadff
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-13 13:51:41 +0200

    5.2
    Using the prototype bean scope

---
+++ ch5/example2/src/main/java/com/example/ProjectConfig.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import com.example.services.CommentService;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public CommentService commentService() {
+        return new CommentService();
+    }
+}
