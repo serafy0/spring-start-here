524e939ba5a497e540048a32693bc8549c747621
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-22 04:38:44 +0200

    8.1.2 Using request parameters to send data from client to server

--- ch8/example/src/main/java/com/example/example/MainController.java
+++ ch8/example/src/main/java/com/example/example/MainController.java
@@ -3,14 +3,17 @@ package com.example.example;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home(Model page) {
-        page.addAttribute("username", "Sam");
-        page.addAttribute("color", "red");
+    public String home(@RequestParam(defaultValue = "red") String color,
+            @RequestParam(required = false) String name,
+            Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
 
         return "welcome.html";
     }
