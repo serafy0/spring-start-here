801de05caa474ba31cfa134462b048f7e6cfc94f
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-24 05:25:20 +0200

    9.3
    Using the application scope in a Spring web app

--- ch9/example/src/main/java/com/example/example/controllers/MainController.java
+++ ch9/example/src/main/java/com/example/example/controllers/MainController.java
@@ -6,21 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.example.services.LoggedUserManagementService;
+import com.example.example.services.LoginCountService;
 
 @Controller
 public class MainController {
 
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
     public MainController(
-            LoggedUserManagementService loggedUserManagementService) {
+            LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+
     }
 
     @GetMapping("/main")
     public String home(@RequestParam(required = false) String logout, Model model) {
         String username = loggedUserManagementService.getUsername();
-
+        int count = loginCountService.getCount();
         if (logout != null) {
             loggedUserManagementService.setUsername(null);
         }
@@ -28,6 +32,7 @@ public class MainController {
             return "redirect:/";
         }
         model.addAttribute("username", username);
+        model.addAttribute("loginCount", count);
         return "main.html";
     }
 }
--- ch9/example/src/main/java/com/example/example/processors/LoginProcessor.java
+++ ch9/example/src/main/java/com/example/example/processors/LoginProcessor.java
@@ -4,21 +4,26 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import com.example.example.services.LoggedUserManagementService;
+import com.example.example.services.LoginCountService;
 
 @Component
 @RequestScope
 public class LoginProcessor {
 
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
     private String username;
     private String password;
 
-    public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService,
+            LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     public boolean login() {
+        loginCountService.increment();
         String username = this.getUsername();
         String password = this.getPassword();
 
