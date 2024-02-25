4ecd38a2d4f6d5d7263e1dce2938a8e6dd3c6e52
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-25 09:58:37 +0200

    10.3.2
    Setting the response status and headers

--- ch10/example/src/main/java/com/example/controllers/CountryController.java
+++ ch10/example/src/main/java/com/example/controllers/CountryController.java
@@ -2,6 +2,10 @@ package com.example.controllers;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +14,14 @@ import com.example.models.Country;
 @RestController
 public class CountryController {
     @GetMapping("/france")
-    public Country france() {
+    public ResponseEntity<Country> france() {
         Country c = Country.of("France", 67);
 
-        return c;
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital", "Paris")
+                .header("favorite_food", "cheese and wine")
+                .body(c);
 
     }
 
