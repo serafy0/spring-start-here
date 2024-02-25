b745f4f39d901868c15e627521ee5c1e2991287f
Mohamed El-Serafy <elserafy02@gmail.com>
2024-02-25 10:55:28 +0200

    10.4
    Using a request body to get data from the client

--- ch10/example/src/main/java/com/example/controllers/PaymentController.java
+++ ch10/example/src/main/java/com/example/controllers/PaymentController.java
@@ -1,8 +1,11 @@
 package com.example.controllers;
 
+import java.util.logging.Logger;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.exceptions.NotEnoughMoneyException;
@@ -14,12 +17,15 @@ import com.example.services.PaymentService;
 public class PaymentController {
     private final PaymentService paymentService;
 
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
+
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
-    @PostMapping("/payment")
+    @PostMapping("/oldpayment")
     public ResponseEntity<?> makResponseEntity() {
+
         try {
             PaymentDetails paymentDetails = paymentService.processPayment();
 
@@ -34,4 +40,14 @@ public class PaymentController {
         }
 
     }
+
+    @PostMapping("/payment")
+    public ResponseEntity<PaymentDetails> makePayment(
+            @RequestBody PaymentDetails paymentDetails) {
+        logger.info("Recived payment " + paymentDetails.getAmount());
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
+    }
+
 }
